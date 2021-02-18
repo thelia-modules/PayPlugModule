552cc88a19f86610e40834587ab7cd7d7b6ae928
Nicolas <nicolas@omnitic.com>
2021-02-18 10:04:55 +0100

    Various typos and translations fixes (#6)
    Co-authored-by: Vincent Lopes-Vicente <vlopesvicente@gmail.com>

--- I18n/frontOffice/default/fr_FR.php
+++ I18n/frontOffice/default/fr_FR.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'A payment card ending by %last4 has been previously saved at your request. Payment for your order will be made immediately after clicking the "Next Step" button.' => 'Une carte de paiement finissant par %last4 as été enregitrée précédemment a votre demande. Le paiement de cette commande seras fait automatiquement après un clique sur le bouton "Etape suivante".',
-    'One click payment is available !' => 'Paiement en un clique disponible !',
+    'A payment card ending by %last4 has been previously saved at your request. Payment for your order will be made immediately after clicking the "Next Step" button.' => 'Une carte de paiement finissant par %last4 as été enregitrée précédemment a votre demande. Le paiement de cette commande seras fait automatiquement après un clic sur le bouton "Etape suivante".',
+    'One click payment is available !' => 'Paiement en un clic disponible !',
     'Pay in %n times without fees.' => 'Paiement en %n fois sans frais.',
     'To clear your payment card details, <a href="%url">please click here</a>' => 'Pour supprimer ces informations de paiement, <a href="%url">cliquez ici</a>',
     'You will be asked to save your payment card data.' => 'Ils vous sera demandé d\'enregistrer votre carte bancaire.',

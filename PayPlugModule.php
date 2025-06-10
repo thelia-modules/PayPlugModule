aaa2d2b5792cf9273ae5afe3bd828a874a13d910
Grégory Laissus <glaissus@openstudio.fr>
2025-06-10 17:35:46 +0200

    feat: thelia 2.6 autowiring

--- PayPlugModule.php
+++ PayPlugModule.php
@@ -154,7 +154,7 @@ class PayPlugModule extends AbstractPaymentModule
     public static function configureServices(ServicesConfigurator $servicesConfigurator): void
     {
         $servicesConfigurator->load(self::getModuleCode().'\\', __DIR__)
-            ->exclude([THELIA_MODULE_DIR . ucfirst(self::getModuleCode()). "/I18n/*"])
+            ->exclude(["/I18n/*"])
             ->autowire(true)
             ->autoconfigure(true);
     }

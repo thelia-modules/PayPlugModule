0a296135961966ade0303414fdcd94f2473c5919
lnormandon <lnormandon@openstudio.fr>
2025-06-23 16:37:57 +0200

    Save refund notification id (avoid duplicate refund)

--- Config/TheliaMain.sql
+++ Config/TheliaMain.sql
@@ -93,5 +93,26 @@ CREATE TABLE `pay_plug_module_delivery_type`
         ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
+-- ---------------------------------------------------------------------
+-- pay_plug_notification_history
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `pay_plug_notification_history`;
+
+CREATE TABLE `pay_plug_notification_history`
+(
+    `uuid` VARCHAR(150) NOT NULL,
+    `order_id` INTEGER NOT NULL,
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
+    PRIMARY KEY (`uuid`,`order_id`),
+    INDEX `pay_plug_notification_history_fi_75704f` (`order_id`),
+    CONSTRAINT `pay_plug_notification_history_fk_75704f`
+        FOREIGN KEY (`order_id`)
+        REFERENCES `order` (`id`)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE=InnoDB;
+
 # This restores the fkey checks, after having unset them earlier
 SET FOREIGN_KEY_CHECKS = 1;
